a075f4c7b1563bef500999a42de497a3e73d583d
Leon Mergen <leon@solatis.com>
2014-12-30 03:48:44 +0100

    Now wrapping all network operations inside a ResourceT, to ensure proper release of sockets when we're done.

--- src/Dissent/Network.hs
+++ src/Dissent/Network.hs
@@ -9,43 +9,51 @@
 module Dissent.Network where
 
 import Control.Concurrent (threadDelay)
-import Control.Exception
+import Control.Exception (IOException)
+import Control.Exception.Lifted (try)
 
-import qualified Network.Socket as NS
+import Control.Monad.Trans.Resource
+import Control.Monad.Trans.Class (lift)
 
-import qualified Dissent.Internal.Debug as D
-import qualified Dissent.Types          as T
-import qualified Dissent.Quorum         as Q
+import qualified Network.Socket               as NS
 
-createSocket :: NS.Family -> IO NS.Socket
-createSocket family =
-  D.log ("Creating new socket") (NS.socket family NS.Stream NS.defaultProtocol)
+import qualified Dissent.Internal.Debug       as D
+import qualified Dissent.Types                as T
+import qualified Dissent.Quorum               as Q
+
+createSocket :: NS.Family -> ResourceT IO NS.Socket
+createSocket family = do
+  (_releaseKey, socket) <- allocate
+    (NS.socket family NS.Stream NS.defaultProtocol)
+    (NS.close)
+
+  return (socket)
+
 
 -- | Binds a socket for listening onto a certain address
-listenSocket :: NS.PortNumber -- ^ The port we'll be listening at
-             -> IO NS.Socket     -- ^ Our resulting socket
+listenSocket :: NS.PortNumber          -- ^ The port we'll be listening at
+             -> ResourceT IO NS.Socket -- ^ Our resulting socket
 listenSocket port =
   let addr  = NS.SockAddrInet6 port 0 NS.iN6ADDR_ANY 0
 
   in do
     socket  <- createSocket NS.AF_INET6
-    _       <- NS.bind socket addr
-    _       <- NS.listen socket 5
+    lift $ NS.bind   socket addr
+    lift $ NS.listen socket 5
 
     D.log ("Now listening on socket: " ++ show socket) (return (socket))
 
 -- | Accepts a single socket. This is a blocking operation.
-acceptConnection :: NS.Socket                   -- ^ Server socket to accept connections from.
-                                                --   This socket must be already bound using listenSocket.
-                 -> IO (NS.Socket, NS.SockAddr) -- ^ Our accepted connection
-acceptConnection = NS.accept
-
+acceptConnection :: NS.Socket                             -- ^ Server socket to accept connections from.
+                                                          --   This socket must be already bound using listenSocket.
+                 -> ResourceT IO (NS.Socket, NS.SockAddr) -- ^ Our accepted connection
+acceptConnection = lift . NS.accept
 
 -- | Utility function that allows us to connect to a remote host, and returns an
 --   Either if there is a connection failure.
 --
 --   This is a blocking operation.
-connectSocket :: NS.HostName -> NS.PortNumber -> IO (Either IOException (NS.Socket, NS.SockAddr))
+connectSocket :: NS.HostName -> NS.PortNumber -> ResourceT IO (Either IOException (NS.Socket, NS.SockAddr))
 connectSocket host port =
   let portToService :: NS.PortNumber -> NS.ServiceName
       portToService = show
@@ -58,19 +66,18 @@ connectSocket host port =
       sockAddr :: NS.AddrInfo -> NS.SockAddr
       sockAddr addr = NS.addrAddress addr
 
-      connect :: IO (NS.Socket, NS.SockAddr)
+      connect :: ResourceT IO (NS.Socket, NS.SockAddr)
       connect = do
-        info   <- addrInfo
+        info   <- lift $ addrInfo
         socket <- createSocket (NS.addrFamily info)
 
         let addr = sockAddr info
 
-        D.log ("Now connecting to socket at address: " ++ show addr) (NS.connect socket addr)
+        D.log ("Now connecting to socket at address: " ++ show addr) (lift $ NS.connect socket addr)
 
         return (socket, addr)
 
-
-  in try (connect) :: IO (Either IOException (NS.Socket, NS.SockAddr))
+  in try connect
 
 
 -- | Determines what host/port to start accepting connections on, accepts
@@ -78,9 +85,9 @@ connectSocket host port =
 --   the amount of connections you wish to accept.
 --
 --   This is a blocking operation.
-quorumAccept :: T.Quorum                      -- ^ The Quorum we need to start accepting connections from
-             -> Int                           -- ^ How many connections do we accept ?
-             -> IO [(NS.Socket, NS.SockAddr)] -- ^ The sockets we accepted
+quorumAccept :: T.Quorum                                -- ^ The Quorum we need to start accepting connections from
+             -> Int                                     -- ^ How many connections do we accept ?
+             -> ResourceT IO [(NS.Socket, NS.SockAddr)] -- ^ The sockets we accepted
 quorumAccept quorum num =
 
       -- This function helps us determine the socket address other nodes knows us by
@@ -91,13 +98,12 @@ quorumAccept quorum num =
     serverSocket  <- listenSocket (T.port localAddr)
     connections   <- sequence (replicate num (acceptConnection serverSocket))
 
-    D.log ("Closing server socket") (NS.close serverSocket)
     return (connections)
 
 -- | Similar like quorumAccept, but accepts only a single connection. Useful for
 --   non-leaders in the Quorum.
-quorumAcceptOne :: T.Quorum                -- ^ The quorum we accept connections from
-                -> IO (NS.Socket, NS.SockAddr) -- ^ All the connections we accept
+quorumAcceptOne :: T.Quorum                              -- ^ The quorum we accept connections from
+                -> ResourceT IO (NS.Socket, NS.SockAddr) -- ^ All the connections we accept
 quorumAcceptOne quorum = (return . head) =<< quorumAccept quorum 1
 
 -- | Data structure that either represents an Infinite amount of reconnects, or
@@ -109,29 +115,29 @@ data ConnectAttempts
 
 -- | Connects to our downstream node. If the server is not available, keeps retrying
 quorumConnect :: T.Quorum -- ^ The Quorum we need to lookup our downstream peer from
-              -> IO (Either String (NS.Socket, NS.SockAddr))
+              -> ResourceT IO (Either String (NS.Socket, NS.SockAddr))
 quorumConnect quorum = quorumConnect' quorum (Infinity) 100000
 
 -- | Implementation of quorumConnect function, with extensive parameters
 quorumConnect' :: T.Quorum        -- ^ The Quorum we need to lookup our downstream peer from
                -> ConnectAttempts -- ^ The amount of times we will try to connect
                -> Int             -- ^ The delay (in microseconds) between retries
-               -> IO (Either String (NS.Socket, NS.SockAddr))
+               -> ResourceT IO (Either String (NS.Socket, NS.SockAddr))
 quorumConnect' quorum retries delay =
   let lookupNextPeer :: T.Address
       lookupNextPeer = T.addr (Q.lookupPeer quorum (Q.successorId quorum))
 
-      connectLoop :: ConnectAttempts -> IO (Either String (NS.Socket, NS.SockAddr))
+      connectLoop :: ConnectAttempts -> ResourceT IO (Either String (NS.Socket, NS.SockAddr))
       connectLoop (Attempts 0) = return (Left ("Unable to connect to remote"))
       connectLoop attemptsLeft =
 
-        let handler :: Either IOException (NS.Socket, NS.SockAddr) -> IO (Either String (NS.Socket, NS.SockAddr))
+        let handler :: Either IOException (NS.Socket, NS.SockAddr) -> ResourceT IO (Either String (NS.Socket, NS.SockAddr))
             handler (Right result) = return (Right result)
             handler (Left _) = do
               -- This means the remote host was not (yet) available, and we should retry
               D.log
                 ("Unable to connect to " ++ show lookupNextPeer ++ ", sleeping for " ++ show delay ++ " microseconds, attempt = " ++ show attemptsLeft)
-                (threadDelay delay)
+                (lift $ threadDelay delay)
 
               case attemptsLeft of
                Attempts i -> connectLoop (Attempts (i - 1))
--- test/Dissent/NetworkSpec.hs
+++ test/Dissent/NetworkSpec.hs
@@ -2,9 +2,12 @@ module Dissent.NetworkSpec where
 
 import Data.Either (isRight)
 
-import Control.Concurrent (forkIO,
-                           killThread,
-                           threadDelay)
+import Control.Concurrent (threadDelay)
+
+import Control.Monad.Trans.Resource ( runResourceT
+                                    , resourceForkIO)
+import Control.Monad.Trans.Class (lift)
+
 
 import qualified Dissent.Quorum  as Q(initialize)
 import qualified Dissent.Network as N
@@ -21,58 +24,49 @@ spec :: Spec
 spec = do
   describe "launching the quorum accept loop" $ do
 
-    it "listens on the correct address" $ do
+    it "listens on the correct address" $ runResourceT $ do
 
       let addr   = "127.0.0.1"
           port   = 1234
           quorum = fromRight (Q.initialize [U.addressStub "0.0.0.0" port, U.addressStub addr port, U.addressStub "0.0.0.1" port] (U.addressStub addr port))
 
-      threadId <- forkIO $ do
+      _ <- resourceForkIO $ do
         socket <- N.quorumAcceptOne quorum
-        putStrLn ("Accepted socket: " ++ show socket)
-        return ()
+        lift $ putStrLn ("Accepted socket: " ++ show socket)
 
-      putStrLn "Created quorum that accepts connections.."
+      lift $ putStrLn "Created quorum that accepts connections.."
 
-      threadDelay 100000
+      lift $ threadDelay 100000
       socket <- N.connectSocket "127.0.0.1" 1234
 
-      putStrLn "Connected to socket.."
+      lift $ putStrLn "Connected to socket.."
 
-      -- Blocks until the var is written to
-      socket       `shouldSatisfy` isRight
+      lift $ (socket `shouldSatisfy` isRight)
 
-      killThread threadId
-
-
-    it "also accepts ipv6 connections" $ do
+    it "also accepts ipv6 connections" $ runResourceT $ do
 
       -- Notice how our own address is "127.0.0.1" ..
       let addr   = "127.0.0.1"
           port   = 1235
           quorum = fromRight (Q.initialize [U.addressStub "0.0.0.0" port, U.addressStub addr port, U.addressStub "0.0.0.1" port] (U.addressStub addr port))
 
-      threadId <- forkIO $ do
+      _ <- resourceForkIO $ do
         socket <- N.quorumAcceptOne quorum
-        putStrLn ("Accepted socket: " ++ show socket)
-        return ()
+        lift $ putStrLn ("Accepted socket: " ++ show socket)
 
-      putStrLn "Created quorum that accepts connections.."
-
-      threadDelay 100000
+      lift $ putStrLn "Created quorum that accepts connections.."
+      lift $ threadDelay 100000
 
       -- .. but we connect to ::1
       socket <- N.connectSocket "::1" 1235
 
-      putStrLn "Connected to socket.."
+      lift $ putStrLn "Connected to socket.."
 
       -- Blocks until the var is written to
-      socket       `shouldSatisfy` isRight
-
-      killThread threadId
+      lift $ (socket `shouldSatisfy` isRight)
 
   describe "connecting to another node in the quorum" $ do
-    it "fails when the node is not available" $ do
+    it "fails when the node is not available" $ runResourceT $ do
 
       let firstAddress  = U.addressStub "127.0.0.1" 1236
           secondAddress = U.addressStub "127.0.0.1" 1237
@@ -81,9 +75,9 @@ spec = do
           quorum   = fromRight (Q.initialize addresses firstAddress)
 
       result <- N.quorumConnect' quorum (N.Attempts 1) 1000000
-      result `shouldBe` Left "Unable to connect to remote"
+      lift $ (result `shouldBe` Left "Unable to connect to remote")
 
-    it "succeeds when the node is available" $ do
+    it "succeeds when the node is available" $ runResourceT $ do
       let firstAddress  = U.addressStub "127.0.0.1" 1238
           secondAddress = U.addressStub "127.0.0.1" 1239
           addresses     = [firstAddress, secondAddress]
@@ -91,30 +85,10 @@ spec = do
           firstQuorum   = fromRight (Q.initialize addresses firstAddress)
           secondQuorum  = fromRight (Q.initialize addresses secondAddress)
 
-      thread <- forkIO $ do
-                _ <- N.quorumAcceptOne firstQuorum
-                return ()
+      _ <- resourceForkIO $ do
+        socket <- N.quorumAcceptOne firstQuorum
+        lift $ putStrLn ("Accepted socket: " ++ show socket)
+
       result <- N.quorumConnect secondQuorum
 
-      isRight (result) `shouldBe` True
-
-      killThread thread
-
-
-    it "fails when trying to connect to the same node multiple times" $ do
-      let firstAddress  = U.addressStub "127.0.0.1" 1240
-          secondAddress = U.addressStub "127.0.0.1" 1241
-          addresses     = [firstAddress, secondAddress]
-
-          firstQuorum   = fromRight (Q.initialize addresses firstAddress)
-          secondQuorum  = fromRight (Q.initialize addresses secondAddress)
-
-      thread <- forkIO $ do
-                _ <- N.quorumAcceptOne firstQuorum
-                return ()
-
-      _ <- N.quorumConnect secondQuorum
-      result <- N.quorumConnect' secondQuorum (N.Attempts 10) 100000
-      result `shouldBe` Left "Unable to connect to remote"
-
-      killThread thread
+      lift $ (isRight (result) `shouldBe` True)
