e6215c82998a0d6f7d46a492b537d971c9dcb6ae
Leon Mergen <leon@solatis.com>
2014-12-31 10:50:45 +0100

    Adds additional leader election test cases for Quorum

--- test/Dissent/QuorumSpec.hs
+++ test/Dissent/QuorumSpec.hs
@@ -1,13 +1,15 @@
 module Dissent.QuorumSpec where
 
-import Dissent.Quorum (initialize, predecessorId, successorId)
+import           Dissent.Quorum (initialize, predecessorId, successorId)
 
-import Data.Either (isLeft)
-import qualified Dissent.Types as T
-import qualified Data.Vector   as V
+import           Data.Either    (isLeft)
 
-import qualified Dissent.Util  as U
-import Test.Hspec
+import qualified Data.Vector    as V
+import qualified Dissent.Quorum as Q
+import qualified Dissent.Types  as T
+
+import qualified Dissent.Util   as U
+import           Test.Hspec
 
 spec :: Spec
 spec = do
@@ -30,14 +32,33 @@ spec = do
                                             T.peerDefault 1 (U.remoteStub "0.0.0.1" 1234),
                                             T.peerDefault 2 (U.remoteStub "0.0.0.2" 1234)]))
 
-    it "always sorts the quorum's peers based on hostname" $
+    it "always sorts the quorum's peers based on hostname/port" $
       (initialize
-       [U.remoteStub "0.0.0.2" 1234, U.remoteStub "0.0.0.1" 1234, U.remoteStub "0.0.0.0" 1234]
-       (U.remoteStub "0.0.0.2" 1234))
+       [U.remoteStub "0.0.0.1" 1234, U.remoteStub "0.0.0.1" 1235, U.remoteStub "0.0.0.0" 1234]
+       (U.remoteStub "0.0.0.1" 1235))
       `shouldBe`
       Right (T.quorumDefault 2 (V.fromList [T.peerDefault 0 (U.remoteStub "0.0.0.0" 1234),
                                             T.peerDefault 1 (U.remoteStub "0.0.0.1" 1234),
-                                            T.peerDefault 2 (U.remoteStub "0.0.0.2" 1234)]))
+                                            T.peerDefault 2 (U.remoteStub "0.0.0.1" 1235)]))
+
+  describe "lookup up the leader" $ do
+    it "always selects the quorum's first peer as leader" $
+      let quorum = U.fromRight (initialize
+                                [ U.remoteStub "0.0.0.2" 1234
+                                , U.remoteStub "0.0.0.1" 1234
+                                , U.remoteStub "0.0.0.0" 1234 ]
+                                (U.remoteStub "0.0.0.2" 1234))
+
+      in T.leaderId quorum `shouldBe` 0
+
+    it "looks up the correct Peer as leader" $
+      let quorum = U.fromRight (initialize
+                                [ U.remoteStub "0.0.0.2" 1234
+                                , U.remoteStub "0.0.0.1" 1234
+                                , U.remoteStub "0.0.0.0" 1234 ]
+                                (U.remoteStub "0.0.0.2" 1234))
+
+      in T.id (Q.lookupLeaderPeer (quorum)) `shouldBe` T.leaderId quorum
 
   describe "looking up a successor" $ do
     it "should return self in a single-host quorum" $
